52791775584704acb3a691b884abc52ceba34411
Jonathan Nicholas <jojonicho181@gmail.com>
2020-05-29 02:23:49 +0700

    :fire: migrate from apollo-boost to apollo-client to use apollo-link-token-refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Routes } from "./Routes";
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
+
 import { getAccessToken } from "./accessToken";
 import { App } from "./App";
 
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      let handle: any;
+      Promise.resolve(operation)
+        .then((operation) => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `bearer ${accessToken}`,
+              },
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  credentials: "include",
-  request: (operation) => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      operation.setContext({
-        headers: {
-          authorization: `bearer ${accessToken}`,
-        },
-      });
-    }
-  },
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      console.log(graphQLErrors);
+      console.log(networkError);
+      // if (graphQLErrors) {
+      //   sendToLoggingService(graphQLErrors);
+      // }
+      // if (networkError) {
+      //   logoutUser();
+      // }
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:4000/graphql",
+      credentials: "include",
+    }),
+  ]),
+  cache,
 });
 
 ReactDOM.render(
--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -3,12 +3,14 @@ import { useMeQuery } from "../generated/graphql";
 
 interface MeProps {}
 
-export const Me: React.FC<MeProps> = ({}) => {
-  const { data, error, loading } = useMeQuery();
+export const Me: React.FC<MeProps> = () => {
+  const { data, error, loading } = useMeQuery({
+    fetchPolicy: "network-only",
+  });
   const Container = loading ? (
     <div>Loading...</div>
   ) : error ? (
-    <div>err</div>
+    <div>{error}</div>
   ) : data ? (
     <div>{data.me}</div>
   ) : (
